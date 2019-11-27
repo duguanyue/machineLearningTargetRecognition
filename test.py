b671a01a3539fb3d9efd3f3de53ef54f0374176a
xuzhang <xzhcn6@outlook.com>
2019-11-28 01:08:26 +0800

    yolo

---
+++ test.py
@@ -0,0 +1,9 @@
+from SIXray import SIXrayDetection
+train_sets = "/Users/xuzhang/PycharmProjects/UnbalancedSamples/coreless_5000.txt"
+val_sets = "val"
+test_sets = "test"
+
+sd = SIXrayDetection(image_sets=train_sets)
+
+tp201, target, height, width, og_img = sd.pull_item(0)
+print(tp201, target, height, width, og_img)
