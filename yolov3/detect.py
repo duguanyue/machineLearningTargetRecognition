9aa8369a27a5d9d3e7f76248bce3d305cb328e0b
duguanyue <duguanyue@gmail.com>
2019-12-06 11:36:18 +0800

    修改目录地址

--- yolov3/detect.py
+++ yolov3/detect.py
@@ -19,16 +19,34 @@ def arg_parse():
     Parse arguements to the detect module
     """
 
-    parser = argparse.ArgumentParser(description='YOLO v3 检测模型')
+    parser = argparse.ArgumentParser(description="YOLO v3 检测模型")
 
-    parser.add_argument("--images", dest='images', help="待检测图像目录", default="/Users/hehongbo/Downloads/小样本", type=str)
-    parser.add_argument("--det", dest='det', help="检测结果保存目录", default="/Users/hehongbo/Downloads/YOLO结果", type=str)
+    parser.add_argument(
+        "--images",
+        dest="images",
+        help="待检测图像目录",
+        default="path/to your sample",
+        type=str,
+    )
+    parser.add_argument(
+        "--det", dest="det", help="检测结果保存目录", default="path/to your save dir", type=str
+    )
     parser.add_argument("--bs", dest="bs", help="Batch size，默认为1", default=1)
-    parser.add_argument("--confidence", dest="confidence", help="目标检测结果置信度阈值", default=0.5)
-    parser.add_argument("--nms_thresh", dest="nms_thresh", help="NMS非极大值抑制阈值", default=0.4)
-    parser.add_argument("--cfg", dest='cfgfile', help="配置文件", default="cfg/yolov3.cfg", type=str)
-    parser.add_argument("--weights", dest='weightsfile', help="模型权重", default="yolov3.weights", type=str)
-    parser.add_argument("--reso", dest='reso', help="网络输入分辨率.分辨率越高,则准确率越高;反之亦然", default="416", type=str)
+    parser.add_argument(
+        "--confidence", dest="confidence", help="目标检测结果置信度阈值", default=0.5
+    )
+    parser.add_argument(
+        "--nms_thresh", dest="nms_thresh", help="NMS非极大值抑制阈值", default=0.4
+    )
+    parser.add_argument(
+        "--cfg", dest="cfgfile", help="配置文件", default="cfg/yolov3.cfg", type=str
+    )
+    parser.add_argument(
+        "--weights", dest="weightsfile", help="模型权重", default="yolov3.weights", type=str
+    )
+    parser.add_argument(
+        "--reso", dest="reso", help="网络输入分辨率.分辨率越高,则准确率越高;反之亦然", default="416", type=str
+    )
 
     return parser.parse_args()
 
@@ -42,7 +60,7 @@ start = 0
 CUDA = torch.cuda.is_available()
 
 num_classes = 2
-classes = ['带芯充电宝', '无芯充电宝']
+classes = ["带芯充电宝", "无芯充电宝"]
 
 # Set up the neural network
 print("Loading network.....")
@@ -65,10 +83,10 @@ model.eval()
 read_dir = time.time()
 # Detection phase
 try:
-    imlist = [osp.join(osp.realpath('.'), images, img) for img in os.listdir(images)]
+    imlist = [osp.join(osp.realpath("."), images, img) for img in os.listdir(images)]
 except NotADirectoryError:
     imlist = []
-    imlist.append(osp.join(osp.realpath('.'), images))
+    imlist.append(osp.join(osp.realpath("."), images))
 except FileNotFoundError:
     print("No file or directory with the name {}".format(images))
     exit()
@@ -84,13 +102,17 @@ im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
 im_dim_list = torch.FloatTensor(im_dim_list).repeat(1, 2)
 
 leftover = 0
-if (len(im_dim_list) % batch_size):
+if len(im_dim_list) % batch_size:
     leftover = 1
 
 if batch_size != 1:
     num_batches = len(imlist) // batch_size + leftover
-    im_batches = [torch.cat((im_batches[i * batch_size: min((i + 1) * batch_size,
-                                                            len(im_batches))])) for i in range(num_batches)]
+    im_batches = [
+        torch.cat(
+            (im_batches[i * batch_size : min((i + 1) * batch_size, len(im_batches))])
+        )
+        for i in range(num_batches)
+    ]
 
 write = 0
 
@@ -112,14 +134,22 @@ for i, batch in enumerate(im_batches):
 
     if type(prediction) == int:
 
-        for im_num, image in enumerate(imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
+        for im_num, image in enumerate(
+            imlist[i * batch_size : min((i + 1) * batch_size, len(imlist))]
+        ):
             im_id = i * batch_size + im_num
-            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start) / batch_size))
+            print(
+                "{0:20s} predicted in {1:6.3f} seconds".format(
+                    image.split("/")[-1], (end - start) / batch_size
+                )
+            )
             print("{0:20s} {1:s}".format("Objects Detected:", ""))
             print("----------------------------------------------------------")
         continue
 
-    prediction[:, 0] += i * batch_size  # transform the atribute from index in batch to index in imlist
+    prediction[:, 0] += (
+        i * batch_size
+    )  # transform the atribute from index in batch to index in imlist
 
     if not write:  # If we have't initialised output
         output = prediction
@@ -127,10 +157,16 @@ for i, batch in enumerate(im_batches):
     else:
         output = torch.cat((output, prediction))
 
-    for im_num, image in enumerate(imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
+    for im_num, image in enumerate(
+        imlist[i * batch_size : min((i + 1) * batch_size, len(imlist))]
+    ):
         im_id = i * batch_size + im_num
         objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
-        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start) / batch_size))
+        print(
+            "{0:20s} predicted in {1:6.3f} seconds".format(
+                image.split("/")[-1], (end - start) / batch_size
+            )
+        )
         print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
         print("----------------------------------------------------------")
 
@@ -173,13 +209,23 @@ def write(x, results):
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
     cv2.rectangle(img, c1, c2, color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 1);
+    cv2.putText(
+        img,
+        label,
+        (c1[0], c1[1] + t_size[1] + 4),
+        cv2.FONT_HERSHEY_PLAIN,
+        1,
+        [225, 255, 255],
+        1,
+    )
     return img
 
 
 list(map(lambda x: write(x, loaded_ims), output))
 
-det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det, x.split("/")[-1]))
+det_names = pd.Series(imlist).apply(
+    lambda x: "{}/det_{}".format(args.det, x.split("/")[-1])
+)
 
 list(map(cv2.imwrite, det_names, loaded_ims))
 
@@ -191,10 +237,16 @@ print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
 print()
 print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
 print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
-print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) + " images)", output_recast - start_det_loop))
+print(
+    "{:25s}: {:2.3f}".format(
+        "Detection (" + str(len(imlist)) + " images)", output_recast - start_det_loop
+    )
+)
 print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
 print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
-print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch) / len(imlist)))
+print(
+    "{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch) / len(imlist))
+)
 print("----------------------------------------------------------")
 
 torch.cuda.empty_cache()
